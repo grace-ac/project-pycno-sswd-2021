987ddb24c646a7137a44bbcfdca3463031d1c850
Grace Crandall <graceac9@uw.edu>
2022-08-09 11:27:50 -0700

    add working directory

--- code/02-20220809_pycno_fastp.sh
+++ code/02-20220809_pycno_fastp.sh
@@ -15,7 +15,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=graceac9@uw.edu
 ## Specify the working directory for this job
-#SBATCH --chdir=/gscratch/srlab/graceac9/scripts/20220809_pycno_fastp
+#SBATCH --chdir=/gscratch/srlab/graceac9/analyses/pycno/20220809_PSC2021_trimming
 
 ### Fastp for Pycnopodia helianthoides RNAseq
 
